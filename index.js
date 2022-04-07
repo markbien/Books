d248d4dc360b26c113d744e48e9626dd8498f0c8
Mark Danielle Bien <mark.bien@outlook.com>
2022-04-07 10:39:20 +0800

    Re-style project

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 const myLibrary = [];
-const addBookContainer = document.querySelector('.addBookContainer');
-const openAddBookContainer = document.querySelector('.option > .btn');
-const closeAddBookContainer = document.querySelector('.message-box-container > span');
-const bookName = document.querySelector('#bookName');
-const bookAuthor = document.querySelector('#bookAuthor');
-const bookPages = document.querySelector('#bookPages');
-const addBook = document.querySelector('.message-box-buttons > .btn');
+const addBookContainer = document.querySelector(".addBookContainer");
+const openAddBookContainer = document.querySelector(".option > .btn");
+const closeAddBookContainer = document.querySelector(
+  ".message-box-container > span"
+);
+const bookName = document.querySelector("#bookName");
+const bookAuthor = document.querySelector("#bookAuthor");
+const bookPages = document.querySelector("#bookPages");
+const addBook = document.querySelector(".message-box-buttons > .btn");
 
-function toggleMessageBox(){
-  addBookContainer.classList.toggle('show');
-  const inputs = document.querySelectorAll('input');
-  inputs.forEach(input => input.value = "");
+function toggleMessageBox() {
+  addBookContainer.classList.toggle("show");
+  const inputs = document.querySelectorAll("input");
+  inputs.forEach((input) => (input.value = ""));
 }
 
-function addBookToArray(){
-  
+function addBookToArray() {
+  const newBook = new Book(bookName.value, bookAuthor.value, bookPages.value);
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+  toggleMessageBox();
 }
 
-openAddBookContainer.addEventListener('click', toggleMessageBox);
+openAddBookContainer.addEventListener("click", toggleMessageBox);
 
-closeAddBookContainer.addEventListener('click', toggleMessageBox);
+closeAddBookContainer.addEventListener("click", toggleMessageBox);
 
-addBook.addEventListener('click', addBookToArray);
-
+addBook.addEventListener("click", addBookToArray);
 
 // Constructor for Books
-function Book(name, author, pages){
+function Book(name, author, pages) {
   this.name = name;
   this.author = author;
   this.pages = pages;
+  this.hasBeenRead = false;
 }
