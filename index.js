4812e679d721ef528c04e7ab7c8c0aea20de80ca
Mark Danielle Bien <mark.bien@outlook.com>
2022-04-19 14:08:15 +0800

    Add Book Class

--- index.js
+++ index.js
@@ -12,3 +12,52 @@ closeAddBook.addEventListener('click', ()=> {
     addBookContainer.classList.remove('show');
     addBookContainer.querySelectorAll('input').forEach(inp => inp.value ="");
 });
+
+
+
+class Book {
+    #title; 
+    #author;
+    #pages;
+    #status;
+    constructor(title, author, pages){
+        this.#title = title;
+        this.#author = author;
+        this.#pages = pages;
+        this.#status = false;
+    }
+
+    get title(){
+        return this.#title;
+    }
+    
+    get author(){
+        return this.#author;
+    }
+
+    get pages(){
+        return this.#pages;
+    }
+
+    get status(){
+        return this.#status;
+    }
+
+    set status(newVal){
+        this.#status = newVal;
+    }
+
+    showDetails(){
+        console.log(`Book Name: ${this.#title}, Author: ${this.#author}, Pages: ${this.#pages}, Has been read: ${this.#status}`);
+    }
+}
+
+const library = (()=>{
+    const newBook = new Book('H.P.', 'J.K.Rowlins', 1000);
+    newBook.pages = 1500;
+    newBook.showDetails();
+})();
+
+const displayController = (()=>{
+    
+})();
