e4a5b0e1d1b6bd34a9b472b047d1f5395b875b38
Mark Danielle Bien <mark.bien@outlook.com>
2022-04-19 14:46:22 +0800

    Add function to create DOM entries

--- index.js
+++ index.js
@@ -63,15 +63,64 @@ const displayController = (()=>{
         addBookContainer.querySelectorAll('input').forEach(inp => inp.value ="");
     });
 
-    
+    function createBookEntries(){
+        library.showBooks().forEach(book => {
+            createBookDOM(book.title, book.author,  book.pages, book.status);
+        });
+    }
 
-    // const newBook = new Book('H.P.', 'J.K.Rowlins', 1000);
+    function createBookDOM(title, author, pages, status){
+        const cardContainer = $('.card-container');
+        const card = document.createElement('div');
+        card.classList.add('card');
+        const h1Title = document.createElement('h1');
+        const hrFirst = document.createElement('hr');
+        const h3Author = document.createElement('h3');
+        const hrSecond = document.createElement('hr');
+        const h3Pages = document.createElement('h3');
+        const hrThird = document.createElement('hr');
+        const cardOptions = document.createElement('div');
+        cardOptions.classList.add('card-options');
+        const divStatus = document.createElement('div');
+        const labelStatusName = document.createElement('label');
+        labelStatusName.setAttribute('for', 'readStatus')
+        labelStatusName.textContent = "Has been read?";
+        const readStatus = document.createElement('input');
+        readStatus.setAttribute('type', 'checkbox');
+        readStatus.setAttribute('id', 'readStatus');
+        const btnDiv = document.createElement('div');
+        const removeBTN = document.createElement('button');
+        removeBTN.textContent = 'Remove book';
+
+        h1Title.textContent = title;
+        h3Author.textContent = author;
+        h3Pages.textContent = pages;
+        if(status === true) readStatus.checked = true;
+        
+        card.appendChild(h1Title);
+        card.appendChild(hrFirst);
+        card.appendChild(h3Author);
+        card.appendChild(hrSecond);
+        card.appendChild(h3Pages);
+        card.appendChild(hrThird);
+        card.appendChild(cardOptions);
+        cardOptions.appendChild(divStatus);
+        divStatus.appendChild(labelStatusName);
+        divStatus.appendChild(readStatus);
+        cardOptions.appendChild(btnDiv);
+        btnDiv.appendChild(removeBTN);
+        cardContainer.appendChild(card);
+    }
+
+    const newBook = new Book('H.P.', 'J.K.Rowlins', 1000);
     // const newBook2 = new Book('ASDF', 'QWER', 500);
-    // library.addBook(newBook);
+    library.addBook(newBook);
     // library.addBook(newBook2);
     // library.showBooks();
 
     // console.log(library.getBook(0));
     // library.getBook(0).toggleStatus();
     // console.log(library.getBook(0));
+
+    createBookEntries();
 })();
