e38b0bf8bd628d82044c09446569fe71684adfeb
Mark Bien <48498353+markbien@users.noreply.github.com>
2023-09-07 16:36:46 +0800

    Add books using JS instead

--- index.js
+++ index.js
@@ -2,19 +2,8 @@
 const addBookButton = document.querySelector('.addBookContainer');
 const messageBoxBackground = document.querySelector('.messagebox-background')
 const messageBoxCloseButton = document.querySelector('.messagebox span');
-
-addBookButton.addEventListener('click', ()=> {
-  toggleMessageBoxAndAddBookButton();
-});
-
-messageBoxCloseButton.addEventListener('click', ()=>{
-  toggleMessageBoxAndAddBookButton();
-});
-
-function toggleMessageBoxAndAddBookButton(){
-  messageBoxBackground.classList.toggle('show');
-  addBookButton.classList.toggle('hide');
-}
+const addToListButton = document.querySelector('.add-button');
+const booksContainer = document.querySelector('.books-container');
 
 function Book(name, author, hasBeenRead) {
   this.name = name;
@@ -29,3 +18,105 @@ function Book(name, author, hasBeenRead) {
     }
   }
 }
+
+const bookContainer = {
+  container: [],
+  addBookToContainer: function(book){
+    this.container.push(book);
+    createBookInDom(book.name, book.author, book.hasBeenRead);
+  },
+}
+
+window.onload = function(){
+  const book1 = new Book("Harry Potter", "J.K.Rowlins", false);
+  const book2 = new Book("Book2", "Author2", true);
+  const book3 = new Book("Book3", "Author3", false);
+  
+  bookContainer.addBookToContainer(book1);
+  bookContainer.addBookToContainer(book2);
+  bookContainer.addBookToContainer(book3);
+}
+
+function toggleMessageBoxAndAddBookButton(){
+  messageBoxBackground.classList.toggle('show');
+  addBookButton.classList.toggle('hide');
+}
+
+function clearFieldsFromAddBookForm(){
+  const bookName = document.querySelector('#book-name').value;
+  const bookAuthor = document.querySelector('#book-author').value;
+  const readStatus = document.querySelector('#isCompleted').checked;
+
+  bookContainer.addBookToContainer(new Book(bookName, bookAuthor, readStatus));
+}
+
+function createBookInDom(newBookName, newBookAuthor, newBookReadStatus){
+  const bookDiv = document.createElement('div');
+  bookDiv.classList.add('book');
+
+  const bookDetails = document.createElement('div');
+  bookDetails.classList.add('book-details');
+
+  const bookNameLabel = document.createElement('p');
+  bookNameLabel.classList.add("book-name");
+  bookNameLabel.textContent = `${newBookName}`;
+
+  const bookAuthorLabel = document.createElement('p');
+  bookAuthorLabel.classList.add('book-author');
+  bookAuthorLabel.textContent = `${newBookAuthor}`;
+
+  const bookFunctionsDiv = document.createElement('div');
+  bookFunctionsDiv.classList.add('book-functions');
+
+  const bookReadStatusDiv = document.createElement('div');
+  bookReadStatusDiv.classList.add('book-read-status');
+  const completedStatusSpan = document.createElement('span');
+  completedStatusSpan.textContent = "I have completed this:";
+  const completedStatusCheckbox = document.createElement('input');
+  completedStatusCheckbox.type = 'checkbox';
+
+  if (newBookReadStatus) {
+    completedStatusCheckbox.checked = true;
+  } else {
+    completedStatusCheckbox.checked = false;
+  }
+
+  const bookRemoveDiv = document.createElement('div');
+  bookRemoveDiv.classList.add('book-remove');
+  const removeBookButton = document.createElement('input');
+  removeBookButton.type = 'button';
+  removeBookButton.value = "Remove book";
+
+  bookDiv.appendChild(bookDetails);
+  bookDiv.appendChild(bookFunctionsDiv);
+  
+  bookDetails.appendChild(bookNameLabel);
+  bookDetails.appendChild(bookAuthorLabel);
+
+  bookFunctionsDiv.appendChild(bookReadStatusDiv);
+  bookFunctionsDiv.appendChild(bookRemoveDiv);
+
+  bookReadStatusDiv.appendChild(completedStatusSpan);
+  bookReadStatusDiv.appendChild(completedStatusCheckbox);
+
+  bookRemoveDiv.appendChild(removeBookButton);
+
+  booksContainer.appendChild(bookDiv);
+}
+
+addBookButton.addEventListener('click', ()=> {
+  toggleMessageBoxAndAddBookButton();
+});
+
+messageBoxCloseButton.addEventListener('click', ()=>{
+  toggleMessageBoxAndAddBookButton();
+});
+
+
+
+addToListButton.addEventListener('click', function(e){
+  e.preventDefault();
+  clearFieldsFromAddBookForm();
+  toggleMessageBoxAndAddBookButton();
+});
+
