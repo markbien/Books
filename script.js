b5727dd3e76977bd6f2bc3b3467d87c368c05b29
Mark Bien <48498353+markbien@users.noreply.github.com>
2024-08-06 13:43:48 +0800

    Modify the removeBook function to base on bookId (index)

--- script.js
+++ script.js
@@ -5,13 +5,9 @@ const bookshelf = {
   isBookInContainer(book) {
     return this.container.findIndex((item) => item.name === book.name);
   },
-  removeBook(bookToRemove) {
-    const bookIndex = this.isBookInContainer(bookToRemove);
-    if (bookIndex === -1) return;
-    this.container.splice(bookIndex, 1);
-  },
   addBookToShelf(book) {
     this.container.push(book);
+    this.populateBooksInContainer();
   },
   clearContainerDiv() {
     bookContainer.innerHTML = "";
@@ -19,6 +15,8 @@ const bookshelf = {
   createBook(bookObj) {
     const book = document.createElement("div");
     book.classList.add("book");
+    const id = this.isBookInContainer(bookObj);
+    book.setAttribute('id', id);
 
     const bookTitle = document.createElement("h3");
     bookTitle.classList.add("book-title");
@@ -26,11 +24,11 @@ const bookshelf = {
 
     const author = document.createElement("h4");
     author.classList.add("author");
-    author.textContent = bookObj.author;
+    author.textContent = `Book Author: ${bookObj.author}`;
 
     const pages = document.createElement("div");
     pages.classList.add("pages");
-    pages.textContent = `${bookObj.pages} pages`;
+    pages.textContent = `Number of pages: ${bookObj.pages}`;
 
     // FIELDSET START
     const fieldset = document.createElement("fieldset");
@@ -87,11 +85,16 @@ const bookshelf = {
     return book;
   },
   populateBooksInContainer() {
+    this.clearContainerDiv();
     // For each book in container
     this.container.forEach(book => {
-      console.log(book);
-    });
-    // Add book to innerHTML
+      // Add book to innerHTML
+      bookContainer.appendChild(this.createBook(book));      
+    });    
+  },
+  removeBook(bookId) {
+    this.container.splice(bookId, 1);
+    this.populateBooksInContainer();
   },
 };
 
@@ -115,77 +118,6 @@ Book.prototype.changeReadStatus = function (newStatus) {
   }
 };
 
-// function createBook(bookObj) {
-//   const book = document.createElement("div");
-//   book.classList.add("book");
-
-//   const bookTitle = document.createElement("h3");
-//   bookTitle.classList.add("book-title");
-//   bookTitle.textContent = bookObj.name;
-
-//   const author = document.createElement("h4");
-//   author.classList.add("author");
-//   author.textContent = bookObj.author;
-
-//   const pages = document.createElement("div");
-//   pages.classList.add("pages");
-//   pages.textContent = `${bookObj.pages} pages`;
-
-//   // FIELDSET START
-//   const fieldset = document.createElement("fieldset");
-
-//   const legend = document.createElement("legend");
-//   legend.textContent = "Settings";
-
-//   const readStatusContainer = document.createElement("div");
-//   readStatusContainer.classList.add("read-status-container");
-
-//   const labelStatus = document.createElement("label");
-//   labelStatus.textContent = "Status";
-
-//   const selectReadStatus = document.createElement("select");
-//   selectReadStatus.setAttribute("name", "read-status");
-//   selectReadStatus.classList.add("options");
-
-//   const pending = document.createElement("option");
-//   pending.setAttribute("value", "pending");
-//   pending.textContent = "Pending";
-
-//   const ongoing = document.createElement("option");
-//   ongoing.setAttribute("value", "ongoing");
-//   ongoing.textContent = "Ongoing";
-
-//   const finished = document.createElement("option");
-//   finished.setAttribute("value", "finished");
-//   finished.textContent = "Finished";
-
-//   const abandoned = document.createElement("option");
-//   abandoned.setAttribute("value", "abandoned");
-//   abandoned.textContent = "Abandoned";
-
-//   const btn = document.createElement("button");
-//   btn.textContent = "Delete";
-
-//   selectReadStatus.appendChild(pending);
-//   selectReadStatus.appendChild(ongoing);
-//   selectReadStatus.appendChild(finished);
-//   selectReadStatus.appendChild(abandoned);
-
-//   fieldset.appendChild(legend);
-//   fieldset.appendChild(readStatusContainer);
-//   readStatusContainer.appendChild(labelStatus);
-//   readStatusContainer.appendChild(selectReadStatus);
-//   fieldset.appendChild(btn);
-//   // FIELDSET END
-
-//   book.appendChild(bookTitle);
-//   book.appendChild(author);
-//   book.appendChild(pages);
-//   book.appendChild(fieldset);
-
-//   return book;
-// }
-
 const harryPotter = new Book(
   "Harry Potter and the Sorcerer's Stone",
   "J.K. Rowlins",
@@ -196,7 +128,5 @@ const random = new Book("Random", "Rand", 1000);
 
 bookshelf.addBookToShelf(harryPotter);
 bookshelf.addBookToShelf(bible);
-
-// bookContainer.appendChild(bookshelf.createBook(harryPotter));
 
 bookshelf.populateBooksInContainer();
